5c3506b222ef9622bf3ec1d9793d21b23f5500d3
EA - Beatriz Gonçalves De Souza <goncalves_beatriz@educadventista.org>
2023-10-09 14:55:40 -0300

    recapitulação de nodemon

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express()
 
 const caminho = path.join(__dirname, "templates")
 
-app.get('/' (requisiçao, resposta) => {
+app.get('/' , (requisiçao, resposta) => {
      resposta.sendFile(`${caminho}/index.html`)
 })
 
-app.listen(3000, () =>  {
-    console.log("Servidor rodando na porta 3000!")
+app.listen(3004, () =>  {
+    console.log("Servidor rodando na porta 3004!")
 })
